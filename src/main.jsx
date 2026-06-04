29b0c9c6f1f6d85566df0900f8c0ef50c3be08de
melanilozano2015 <melanilozano2015@example.com>
2026-06-04 20:04:25 -0300

    Fix router setup and theme deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,9 @@ import { HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App';
-import Navbar from './components/Navbar';
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
-    <Navbar />
     <App />
   </HashRouter>
 );
